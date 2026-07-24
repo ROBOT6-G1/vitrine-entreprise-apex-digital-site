323eb4807724ac3504a90eabc7a1ee1385c77ea5
ROBOT6-G1 <b.outiquemevasoa@gmail.com>
2026-07-24 22:23:48 +0300

    Update firebase-config.js via DEVWEBIA

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+import { getFirestore, collection, addDoc, getDocs, doc, setDoc, updateDoc, deleteDoc, onSnapshot, query, orderBy } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCikMlADpeQ7RH74RUborBVJ4P81zwAHTE",
+  authDomain: "gen-lang-client-0344726942.firebaseapp.com",
+  projectId: "gen-lang-client-0344726942",
+  storageBucket: "gen-lang-client-0344726942.firebasestorage.app",
+  messagingSenderId: "625686759389",
+  appId: "1:625686759389:web:bd8d94381a001d850a1834"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, collection, addDoc, getDocs, doc, setDoc, updateDoc, deleteDoc, onSnapshot, query, orderBy };
